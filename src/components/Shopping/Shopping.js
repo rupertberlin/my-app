2ffdbdf06406f4bbfbc2e0d748d1ec35637ba9a6
rupertberlin <ulrich.laitenberger@googlemail.com>
2022-03-30 21:48:27 +0200

    main idb-box assembling

--- src/components/Shopping/Shopping.js
+++ src/components/Shopping/Shopping.js
@@ -30,12 +30,27 @@ class Shopping extends Component {
       item: products[0],
       itemsCount: 0,
       total: 0,
-      cart: [],
+      cart: [
+        {
+          product: "ice cream",
+          units: 0,
+          price: 0,
+        },
+        {
+          product: "donuts",
+          units: 0,
+          price: 0,
+        },
+        {
+          product: "beer",
+          units: 0,
+          price: 0,
+        },
+      ],
     };
     this.decrement = this.decrement.bind(this);
     this.increment = this.increment.bind(this);
     this.chooseItem = this.chooseItem.bind(this);
-    this.addToCart = this.addToCart.bind(this);
     this.calculateTotal = this.calculateTotal.bind(this);
     this.sumItems = this.sumItems.bind(this);
     this.removeFromCart = this.removeFromCart.bind(this);
@@ -46,6 +61,7 @@ class Shopping extends Component {
       this.setState((state) => ({
         counter: state.counter - 1,
       }));
+      setTimeout(() => this.updateCart(this.state.item), 105);
     }
   }
 
@@ -53,95 +69,80 @@ class Shopping extends Component {
     this.setState((state) => ({
       counter: state.counter + 1,
     }));
+    setTimeout(() => this.updateCart(this.state.item), 105);
   }
 
   chooseItem(item) {
-    this.setState((state) => ({
-      item: item,
-      counter: 0,
-    }));
-  }
-
-  addToCart(item, count) {
-    let allreadyIn = this.state.cart.filter(
-      (insides) => insides.product == item.name
+    setTimeout(
+      () =>
+        this.setState((state) => ({
+          item: item,
+          counter: Number(
+            this.state.cart
+              .filter((e) => e.product === item.name)
+              .map((e) => e.units)
+          ),
+        })),
+      105
     );
-
-    if (allreadyIn.length === 0) {
-      this.setState((state) => ({
-        cart: [
-          ...this.state.cart,
-          { product: item.name, units: state.counter, price: item.price*state.counter},
-        ],
-  
-      }));
-    } else {
-      let pos = this.state.cart.map(function(e) { return e.product; }).indexOf(item.name);
-      let tmpCart = this.state.cart;
-      tmpCart[pos] = {
-        product: item.name,
-        units: tmpCart[pos].units + this.state.counter,
-        price: tmpCart[pos].price + this.state.counter * item.price,
-      }
-      this.setState(state => ({
-        cart: tmpCart,
-      }))
-      
-    }
-    this.setState(state =>({
-      counter: 0,
-    }))
-    setTimeout(() => this.sumItems(), 500);
-    setTimeout(() => this.calculateTotal(), 500);
   }
 
-  goNext(current){
-    switch(current){
-      case 'ice cream':
-        this.setState(state =>({
-          item: products[1],
-        }))
+  updateCart(item) {
+    let pos = this.state.cart
+      .map(function (e) {
+        return e.product;
+      })
+      .indexOf(item.name);
+    let tmpCart = this.state.cart;
+    tmpCart[pos] = {
+      product: item.name,
+      units: this.state.counter,
+      price: this.state.counter * item.price,
+    };
+    this.setState((state) => ({
+      cart: tmpCart,
+    }));
+
+    setTimeout(() => this.sumItems(), 105);
+    setTimeout(() => this.calculateTotal(), 105);
+  }
+
+
+  goNext(current) {
+    switch (current) {
+      case "ice cream":
+        this.chooseItem(products[1]);
         break;
-      case 'donuts':
-        this.setState(state =>({
-          item: products[2],
-        }))
+      case "donuts":
+        this.chooseItem(products[2]);
         break;
-      case 'beer':
-        this.setState(state =>({
+      case "beer":
+        this.setState((state) => ({
           item: products[1],
-        }))
-        break; 
+        }));
+        break;
     }
   }
 
-  removeFromCart(item, count) {
-    
-
-  }
+  removeFromCart(item, count) {}
 
   calculateTotal() {
     let sumCart = this.state.cart.reduce((acc, cartItem, index) => {
-      return (
-        acc+cartItem.price 
-      );
-    },0)
-    this.setState(state => ({
+      return acc + cartItem.price;
+    }, 0);
+    this.setState((state) => ({
       total: sumCart,
-    }))
+    }));
   }
 
   sumItems() {
     let sumItems = this.state.cart.reduce((acc, cartItem, index) => {
-      return (
-        acc+cartItem.units
-      );
-    },0)
-    this.setState(state => ({
+      return acc + cartItem.units;
+    }, 0);
+    this.setState((state) => ({
       itemsCount: sumItems,
-    }))
+    }));
   }
-
 
   render() {
     return (
@@ -165,19 +166,24 @@ class Shopping extends Component {
           counter={this.state.counter}
           decrement={this.decrement}
           increment={this.increment}
-          addToCart={this.addToCart}
           item={this.state.item}
           goNext={this.goNext}
         />
 
         <div>
           <p>
-          {!this.state.cart ||
-            this.state.cart.
-            filter(e=>e.product===this.state.item.name)
-            .map((item) => item.units + ' ' + this.state.item.name.charAt(0).toUpperCase() + this.state.item.name.slice(1) + ' inside your cart.')
-            }
-            </p>
+            {!this.state.cart ||
+              this.state.cart
+                .filter((e) => e.product === this.state.item.name)
+                .map(
+                  (item) =>
+                    item.units +
+                    " " +
+                    this.state.item.name.charAt(0).toUpperCase() +
+                    this.state.item.name.slice(1) +
+                    " inside your box."
+                )}
+          </p>
         </div>
       </div>
     );
